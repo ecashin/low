6c715af6611c88c6d963d38dde074ab3b0cbf31f
Ed Cashin <ecashin@noserose.net>
2013-09-21 12:16:18 -0400

    Support longer strings with a loop

--- simd-strcmp/main.c
+++ simd-strcmp/main.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 
 #define CPUID_SSE4_2_BIT (20)
 
@@ -24,21 +23,25 @@ void loadstr(char *buf, char *s, size_t m)
 int main(int argc, char *argv[])
 {
 	unsigned long n = (unsigned long) cpuidfn(1);
-	char a[] = "0123456789abcdef";	/* 128-bits worth of data */
-	char b[] = "01c3456789abCdef";
+	char a[512] = "0123456789abcdef";	/* 128-bits worth of data */
+	char b[512] = "01c3456789abCdef";
+	size_t alen = strlen(a);
+	size_t blen = strlen(b);
 
 	if ((n >> CPUID_SSE4_2_BIT & 1) == 0) {
 		fputs("cpuid says this CPU hasn't SSE 4.2\n", stderr);
 		exit(1);
 	}
-	assert(sizeof a == 2*sizeof(unsigned long) + 1 && sizeof a == sizeof b);
-
-	if (argc > 1)
+	if (argc > 1) {
 		loadstr(a, argv[1], sizeof a - 1);
-	if (argc > 2)
+		alen = strlen(a);
+	}
+	if (argc > 2) {
 		loadstr(b, argv[2], sizeof b - 1);
+		blen = strlen(b);
+	}
 	printf("\"%s\" and \"%s\" differ at byte index %lu\n",
-	       a, b, diffidx(a, b, sizeof a - 1));
+	       a, b, diffidx(a, b, alen > blen ? blen : alen));
 
 	return 0;
 }
--- simd-strcmp/subs.s
+++ simd-strcmp/subs.s
@@ -8,11 +8,27 @@ section .text
 diffidx:
 	push rbp
         mov rbp, rsp
-	movdqu xmm0, [rdi]
-	movdqu xmm1, [rsi]
+	add rsp, -8
+	mov [rbp-8], r12	; It's nice to debuggers to store regs on stack.
+	mov r12, 0		; r12 will keep a byte count of how far in we go
+.loop:
+	movdqu xmm0, [rdi]	; load in 128-bits from arg0
+	movdqu xmm1, [rsi]	; load in 128-bits from arg1
 	mov rax, rdx		; len for first operand is rax, len for 2nd is rdx
 	pcmpestri xmm0, xmm1, 24 ; see 325383.pdf p. 661 for this crazy 3rd operand
+	cmp rcx, 16		 ; if they're equal ...
+	jne .end		 ; we're done.
+	add r12, 16
+	add rdx, -16
+	cmp rdx, 0
+	jle .end		; out of data
+	add rdi, 16
+	add rsi, 16
+	jmp .loop
+.end:
+	add rcx, r12
 	mov rax, rcx
+	pop r12
         leave
         ret
 
